99105c04a8044d5be3eb2e78faca5996c2308406
ahmadaims <ahmadbinmunir488@gmail.com>
2024-04-29 14:29:13 +0500

    Chnage the file name from script.js to arithemtic.js to only work that specific file with the arithemtic operators

---
+++ Operators/arithemetic.js
@@ -0,0 +1,32 @@
+/* Addition operator */
+let x = 10;
+let y = 20;
+console.log(`x + y = ${x + y}`);
+console.log(`y - x = ${y - x}`);
+console.log(`x * y = ${x * y}`);
+console.log(`y / x = ${y / x}`);
+
+
+/* Reassigning the variable value */
+/* The modulus operator (%) returns the division remainder. */
+x = 29;
+y = 10;
+console.log(`x % y = ${x % y}`);
+
+
+/*Increament operator */
+x = 10;
+x++;
+console.log(`x = ${x}`);
+/* Decreament Operator */
+x = 5;
+x--;
+console.log(`x = ${x}`);
+
+
+
+/* Exponentiation Operator */
+/* The exponentiation operator (**) raises the first operand to the power of the second operand. */
+a = 5;
+b = 2;
+console.log(`a**b = ${a**b}`);
