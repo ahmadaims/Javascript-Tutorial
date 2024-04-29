73d165deb953088d67f5b7e375f316ab1c2e8bfb
ahmadaims <ahmadbinmunir488@gmail.com>
2024-04-29 19:58:58 +0500

    Add the new file for fuctiions practice in Js

---
+++ Functions/functions.js
@@ -0,0 +1,10 @@
+/* Function Declaration: This is the most common way to define a function. You use the function keyword, followed by a name, parentheses to hold parameters (inputs), curly braces to enclose the function body (the code that executes).  */
+function greet(name) {
+  console.log("Hello, " + name + "!");
+};
+greet("John");
+
+
+function showAlert() { 
+  greet ("Ahmad");
+}
