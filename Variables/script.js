44def20947922759bea4f0829bb1c241020728b6
ahmadaims <ahmadbinmunir488@gmail.com>
2024-04-29 12:03:57 +0500

    Remove the animated change heading code

--- Variables/script.js
+++ Variables/script.js
@@ -6,22 +6,3 @@ console.log(`this is the value of x : ${x}`); // This is a backtick symbol insid
 
 x = 20 
 console.log(`Now the value of x is : '${x}' I reassigned the variable x to 20`);
-
-
-
-
-
-
-
-
-
-// IGNORE THIS PART IF YOU ARE LEARNING FORM BASICS THIS IS JUST CHAGING THE CONTENT OF THE MAIN HEADING FROM "JACASCRIPT VARIABLES" TO "VARIABLE CLASS" AFTER 1000ms(1s)
-console.log('IGNORE THE BELOW ANSWER IF YOU ARE LEARNING FROM BASICS');
-let mainHeading = document.getElementById('main-heading').textContent;
-console.log(mainHeading);
-function changeHeading(){
-	mainHeading = "Variables Class";
-	console.log(mainHeading);
-	document.getElementById('main-heading').textContent = mainHeading;
-}
-const myTimeout = setTimeout(changeHeading, 1000);
