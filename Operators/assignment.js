f63d2df0f2c3ff070ee70f70af06814838d565e6
ahmadaims <ahmadbinmunir488@gmail.com>
2024-04-29 18:19:22 +0500

    Added the complete file of assignment operator with it's Js file.

---
+++ Operators/assignment.js
@@ -0,0 +1,51 @@
+/* Simple Assignment Examples */
+/* The Simple Assignment Operator assigns a value to a variable. */
+let x = 100;
+console.log(`x = ${x}`);
+
+
+/* Addition Assignment Examples */
+/* The Addition Assignment Operator adds a value to a variable. */
+x += 5;
+console.log(`x = ${x}`);
+
+let text = "Hello"; 
+console.log(text);
+text += " World";
+console.log(`Hello + World = ${text}`);
+
+
+
+/* Subtraction Assignment Example */
+/* The Subtraction Assignment Operator subtracts a value from a variable. */
+x = 10;
+x -= 5;
+console.log(`x = ${x}`);
+
+
+/* Multiplication Assignment Example */
+/* The Multiplication Assignment Operator multiplies a variable. */
+x *= 5;
+console.log(`5*5 = ${x}`);
+
+
+/* Exponentiation Assignment Example */
+/* The Exponentiation Assignment Operator raises a variable to the power of the operand. */
+x = 2;
+x **= 5;
+console.log(`2(5) = ${x}`);
+
+
+/* Division Assignment Example */
+/* The Division Assignment Operator divides a variable. */
+let y = 10;
+y /= 2;
+console.log(y);
+
+
+
+/* Remainder Assignment Example */
+/* The Remainder Assignment Operator returns the remainder of a variable divided by the operand. */
+y = 29;
+y %= 10;
+console.log(y);
