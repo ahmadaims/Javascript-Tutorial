8bdece80679504d1d1e656bdd4651e348f386de9
ahmadaims <ahmadbinmunir488@gmail.com>
2024-04-30 12:12:37 +0500

    Practice Javascript functions

--- Functions/functions.js
+++ Functions/functions.js
@@ -4,7 +4,41 @@ function greet(name) {
 };
 greet("John");
 
-
 function showAlert() { 
+  alert ("Ahmad");
   greet ("Ahmad");
 }
+
+
+
+// Multiply       
+function multiply(x, y) { /* parameter 1 and parameter 2 */
+  return x * y;
+}
+console.log(multiply(2, 3));
+
+
+
+
+// fahrenheit to celsius
+let text = "The temperature is " + toCelsius(77) + " Celsius.";
+document.getElementById("demo").innerHTML = text;
+
+function toCelsius(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+} 
+
+
+
+// code here can NOT use carName
+function myFunction() {
+  let carName = "Volvo";
+  // code here CAN use carName
+  console.log(carName);
+}
+myFunction();
+
+
+
+// code below can NOT use carName it will show an error because carName is a local variable inside a function
+console.log(carName);
