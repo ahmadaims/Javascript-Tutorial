9eca70a5009363f0d643688afd5580a9ee552a33
ahmadaims <ahmadbinmunir488@gmail.com>
2024-05-02 14:15:49 +0500

    Define the this keyword - Practice with this keyword

--- Objects/objects.js
+++ Objects/objects.js
@@ -18,7 +18,7 @@ console.log(person["age"]); // 30
 /* You can add new properties or modify existing ones using dot or bracket notation and assigning a value. */
 person.name = "Muhammad";
 person["eyeColor"] = "Blue";
-console.log("Person name is "+person.name + " and his eyes color is " + person.eyeColor); /* Person name is Muhammad and his eyes color is Blue */
+console.log("Person name is " + person.name + " and his eyes color is " + person.eyeColor); /* Person name is Muhammad and his eyes color is Blue */
 
 
 
@@ -32,16 +32,18 @@ console.log(person.city); /* undefined */
 /* Object Methods: */
 /* Objects can also contain methods, which are functions defined within the object that can operate on the object's data. */
 const person2 = {
-  firstName : "Ahmad",
-  greet : function(){
-    console.log("Hello, my name is " + this.firstName); /* Hello, my name is Ahmad */
+  firstName: "Ahmad",
+  lastName: "Munir",
+  fullName: function () {
+    console.log("Hello, my full name is " + this.firstName + " " + this.lastName); /* Hello, my full name is Ahmad Munir */
   }
 }
-person2.greet();
+person2.fullName();
 /*
 In the example above, this refers to the person2 object:
 this.firstName means the firstName property of person2.
-this.lastName means the lastName property of person2.
+this.latName means the lastName property of person2.
+In other words, this.firstName means the firstName property of this object.
 */
 
 
@@ -50,4 +52,10 @@ this.lastName means the lastName property of person2.
 In JavaScript, the this keyword refers to an object.
 Which object depends on how this is being invoked (used or called). 
 The this keyword refers to different objects depending on how it is used:
+this is not a variable. It is a keyword. You cannot change the value of this.
 */
+
+
+/* If you access a method without the () parentheses, it will return the function definition: */
+console.log(person2.fullName);
+
