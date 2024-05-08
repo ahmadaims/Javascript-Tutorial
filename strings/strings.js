21150e5c722add1d9f10743030362eecdcc5d841
ahmadaims <ahmadbinmunir488@gmail.com>
2024-05-08 19:52:38 +0500

    Completed the tutorial for JS strings with example and practice

--- strings/strings.js
+++ strings/strings.js
@@ -5,3 +5,17 @@ console.log(clientName);
 /* String using single quotes */
 let carBrand = 'Toyota';
 console.log(carBrand);
+
+/* Qoutes inside quotes */
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+
+let firstName = `He's often called "Johnny"`;
+console.log(firstName);
+
+let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = abc.length;
+console.log(length);
+
