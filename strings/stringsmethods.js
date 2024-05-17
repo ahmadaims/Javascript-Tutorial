9ec26eade40480d7d005ffe7c1bb5e59a8dc70d5
ahmadaims <ahmadbinmunir488@gmail.com>
2024-05-17 12:53:30 +0500

    Practice and updated stringsmethods

---
+++ strings/stringsmethods.js
@@ -0,0 +1,11 @@
+let indexText = "Please locate where occurs!";
+let indexOf = indexText.lastIndexOf("locate");
+let lastIndexOfText = "Please locate where 'locate' occurs!";
+let lastIndexOf = lastIndexOfText.lastIndexOf("locate");
+console.log(indexOf);
+console.log(lastIndexOf);
+
+
+let abc = "Hello world, welcome to the universe.";
+// Check if a string includes "world":
+console.log(abc.includes("world"));
